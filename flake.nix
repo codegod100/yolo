193db5571d3b868e5b46486e1bf83ee74da6913a
nandi <nandithebull@outlook.com>
2026-03-06 15:07:24 -0800

    Add Garnix binary cache to nixConfig

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "YOLO Greeter - A Kirigami-based greetd login manager";
 
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
